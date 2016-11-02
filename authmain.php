a366ae2e19d501a368de92cac2ea1ee84a23a645
mileswilhelm <wilhelmme72@gmail.com>
2016-11-01 21:13:38 -0400

    fixes log in issue in authmain
    There was an issue (presumably the form not being closed) which
    prevented the user from logging in. This update corrects this.

--- authmain.php
+++ authmain.php
@@ -1,35 +1,29 @@
 <?php
-session_start();
-
-@	$DBhost				=	"localhost";
-@	$DBuser				=	"wilhelmm_admin";
-@	$DBpass				=	"^D20T7LTw]o5";
-@	$DBname				=	"wilhelmm_webdev";
+	session_start();
 	
-
-	if(!isset($_POST['userid']) && !isset($_POST['password']))
+	if (isset($_POST['userid']) && isset($_POST['password']))
 	{
 		// if the user has just tried to log in
-		$userid			=	$_POST['userid'];
-		$password		=	$_POST['password'];
+		$userid		=	$_POST['userid'];
+		$password	=	$_POST['password'];
 		
-		$db_conn		=	new mysqli($DBhost, $DBuser, $DBpass, $DBname);
+		$db_conn	=	new mysqli('localhost', 'wilhelmm_admin', '^D20T7LTw]o5', 'wilhelmm_webdev');
 		
-		if(mysqli_connect_errno())
+		if (mysqli_connect_errno()) 
 		{
 			echo 'Connection to database failed:'.mysqli_connect_error();
 			exit();
 		}
 		
-		$query			=	'select * from authorized_users'
-							."where name='$userid' "
-							." and password=sha1('$password')";
-							
-		$result			=	$db_conn->query($query);
-		if($result->num_rows >0)
+		$query		=	'select * from authorized_users '
+						."where name='$userid' "
+						." and password=sha1('$password')";
+						
+		$result		=	$db_conn->query($query);
+		if ($result->num_rows >0)
 		{
-			//if they are in the database, register the user id
-			$_SESSION['valid_user']	=	$userid;
+			// if they are in the database register the user id
+			$_SESSION['valid_user']		=	$userid;
 		}
 		$db_conn->close();
 	}
@@ -55,43 +49,43 @@ session_start();
 <body>
 <div class="container">
 	<h1 class="display">Home Page</h1>
+	
 <?
-	if(isset($_SESSION['valid_user']))
+	if (isset($_SESSION['valid_user']))
 	{
 		echo '<p>You are logged in as: '.$_SESSION['valid_user'].'</p>';
-		echo '<div><a href="logout.php" class="btn">Log out</a></div>';
+		echo '<a href="logout.php" class="btn">Log out</a>';
 	}
 	else
 	{
-		if(isset($userid))
+		if (isset($userid))
 		{
-			//if they've tried and failed to log in
-			echo '<div class="text-block red"><p class="no-margin">Could not log you in.</p></div>';
+			// if they've tried and failed to log in
+			echo '<div class="text-block red"><p>Could not log you in.</p></div>;';
 		}
 		else
 		{
-			//they have not tried to log in yet or have logged out
-			echo '<p>You are not logged in.</p>';
+			// they have not tried to log in yet or have logged out
+			echo '<div class="text-block yellow"><p>You are not logged in.</p></div>';
 		}
 		
-	//provide form to log in
-	echo '<form method="post" action="authmain.php">';
-	echo '<div class="form-input__container">';
-	echo '<label for="userid" class="form-input_label">Username</label>';
-	echo '<input type="text" class="form-input" id="userid" name="userid">';
-	echo '</div>';
-	echo '<div class="form-input__container">';
-	echo '<label for="password" class="form-input_label">Password</label>';
-	echo '<input type="password" class="form-input" id="password" name="password">';
-	echo '</div>';	
-	echo '<button class="btn btn-block" type="submit">Log In</button>';
-	
+		// provide form to log in
+		echo '<form method="post" action="authmain.php">';
+		echo '<div class="form-input__container">';
+		echo '<label for="userid" class="form-input_label">Username</label>';
+		echo '<input type="text" class="form-input" id="userid" name="userid">';
+		echo '</div>';
+		echo '<div class="form-input__container">';
+		echo '<label for="password" class="form-input_label">Password</label>';
+		echo '<input type="password" class="form-input" id="password" name="password">';
+		echo '</div>';	
+		echo '<button class="btn btn-block" type="submit">Log In</button>';
+		echo '</form>';
 	}
-?>
-<a href="members_only.php" class="btn">Members Section</a>
+?>	
+	<a href="members_only.php" class="btn">Members Section</a>
+		
+	
 </div>
-<script>
-
-</script>
 </body>
 </html>
